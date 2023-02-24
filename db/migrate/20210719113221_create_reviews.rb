eb47b252f8c1dbb432aec5306cdd17b793a5fbeb
Josphat-Kip <josphatkiplimo247@gmail.com>
2023-02-24 06:17:53 +0300

    Done

--- db/migrate/20210719113221_create_reviews.rb
+++ db/migrate/20210719113221_create_reviews.rb
@@ -6,5 +6,11 @@ class CreateReviews < ActiveRecord::Migration[6.1]
       t.integer :game_id # this is our foreign key
       t.timestamps
     end
+    SELECT "users".*
+FROM "users"
+INNER JOIN "reviews"
+  ON "users"."id" = "reviews"."user_id"
+WHERE "reviews"."game_id" = 1
+  end
   end
 end
